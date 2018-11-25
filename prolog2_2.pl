2073181c21783318c2ceab117704aa1dd61aa213
Wowo10 <wojtekwojtek529@gmail.com>
2018-11-25 14:19:50 +0100

    First lab finished.

---
+++ prolog2_2.pl
@@ -0,0 +1,11 @@
+edge(p(1,1),p(2,2)).
+edge(p(1,2),p(2,2)).
+edge(p(2,2),p(2,3)).
+edge(p(2,3),p(3,3)).
+edge(p(2,2),p(3,1)).
+edge(p(3,1),p(2,1)).
+
+dpath(X,Y):-edge(X,Y).
+dpath(X,Y):-edge(X,Z),dpath(Z,Y).
+
+%edge(p(2,1),p(2,2)). makes dpath endless in some cases.
