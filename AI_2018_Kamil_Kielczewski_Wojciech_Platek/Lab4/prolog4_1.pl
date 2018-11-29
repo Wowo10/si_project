c66f8457d2c17fc1cd28565a7e91a074616ec48e
Wowo10 <wojtekwojtek529@gmail.com>
2018-11-30 00:19:29 +0100

    -a
    Finished Lab4

--- AI_2018_Kamil_Kielczewski_Wojciech_Platek/Lab4/prolog4_1.pl
+++ AI_2018_Kamil_Kielczewski_Wojciech_Platek/Lab4/prolog4_1.pl
@@ -8,7 +8,7 @@ concat1([Head|Tail], L1, [Head|L2]) :- concat1(Tail, L1, L2).
 
 %delete1([a,[a,b,c,d,e],X]).
 delete1(Head, [Head|Tail], Tail).
-delete1(X, [Y|Tail],[Y|L1]):-delete(X, Tail, L1).
+delete1(X, [Y|Tail],[Y|L1]):-delete1(X, Tail, L1).
 
 equal(X, X).
 
@@ -31,6 +31,39 @@ rlen(_, 1).
 reverse1([],[]).
 reverse1([Head|Tail], L2):-reverse1(Tail, L1), concat1(L1, [Head], L2).
 
-%reverse2([a,b,c],X).
+%reverse2([a,b,c],[], X).
 reverse2([Head|Tail], Accumulator, L3):-reverse2(Tail, [Head|Accumulator],L3).
 reverse2([],Accumulator,Accumulator).
+
+%sum([1,2,3], X).
+sum([], 0).
+sum([Head|Tail], N):-sum(Tail, N1), N is N1+Head.
+
+%avg([1,2,3,4,5],X).
+avg(List,Avg):-len(List, Len), sum(List, Sum), Avg is Sum/Len.
+
+%only one call
+%count(x,X,3).
+count(_,[],0).
+count(X,[X|Y],Z):-count(X,Y,Z1), Z is Z1+1.
+
+%double([1,2,3],X).
+double([],[]).
+double([H|T],[H,H|Y]) :- double(T,Y). 
+
+help(_,[],0).
+help(Head,[Head|Tail],N):-help(Head,Tail,N1), N is N1+1.
+
+%repeat([x,y,z],X,3).
+repeat([],[],_):-!.
+repeat([H1|T1],L2,N):-repeat(T1,L3,N),help(H1,L4,N),concat1(L4,L3,L2).
+
+sort([]).
+sort([_]).
+sort([X,Y|Tail]):-X=<Y,sort([Y|Tail]).
+
+permutation([],[]).
+permutation([Head|Tail], L):-permutation(Tail, L1), delete1(Head, L, L1).
+
+%naivesort([2,1,9,0,55,2],X).
+naivesort(L1, L2):-permutation(L1, L2), sort(L2).
